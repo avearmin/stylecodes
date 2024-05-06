7dc0c859d57d7394c6771cfbbda567cca2c58324
avearmin <arminradoncic1@gmail.com>
2024-05-06 00:32:42 -0400

    Add bgcolor package and correct codes for color

---
+++ bgcolor/bgcolor.go
@@ -0,0 +1,13 @@
+package bgcolor
+
+const (
+	Black   = "\033[40m"
+	Red     = "\033[41m"
+	Green   = "\033[42m"
+	Yellow  = "\033[43m"
+	Blue    = "\033[44m"
+	Magenta = "\033[45m"
+	Cyan    = "\033[46m"
+	White   = "\033[47m"
+	End     = "\033[49m"
+)
--- color/color.go
+++ color/color.go
@@ -1,13 +1,13 @@
 package color
 
 const (
-	Black   = "\033[40m"
-	Red     = "\033[41m"
-	Green   = "\033[42m"
-	Yellow  = "\033[43m"
-	Blue    = "\033[44m"
-	Magenta = "\033[45m"
-	Cyan    = "\033[46m"
-	White   = "\033[47m"
+	Black   = "\033[30m"
+	Red     = "\033[31m"
+	Green   = "\033[32m"
+	Yellow  = "\033[33m"
+	Blue    = "\033[34m"
+	Magenta = "\033[35m"
+	Cyan    = "\033[36m"
+	White   = "\033[37m"
 	End     = "\033[39m"
 )
