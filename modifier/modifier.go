592d29af73fbffcec4f3e296fe3295571e59558d
avearmin <arminradoncic1@gmail.com>
2024-05-06 03:54:38 -0400

    Edit modifier pkg to use similar structure to others

--- modifier/modifier.go
+++ modifier/modifier.go
@@ -1,17 +1,20 @@
 package modifier
 
-type modifier struct {
-	Start string
-	End   string
-}
-
 var (
-	Bold          = modifier{Start: "\033[1m", End: "\033[22m"}
-	Dim           = modifier{Start: "\033[2m", End: "\033[22m"}
-	Italic        = modifier{Start: "\033[3m", End: "\033[23m"}
-	Underline     = modifier{Start: "\033[4m", End: "\033[24m"}
-	Inverse       = modifier{Start: "\033[7m", End: "\033[27m"}
-	Hidden        = modifier{Start: "\033[8m", End: "\033[28m"}
-	Strikethrough = modifier{Start: "\033[9m", End: "\033[29m"}
-	End           = "\033[0m"
+	Bold          = "\033[1m"
+	Dim           = "\033[2m"
+	Italic        = "\033[3m"
+	Underline     = "\033[4m"
+	Inverse       = "\033[7m"
+	Hidden        = "\033[8m"
+	Strikethrough = "\033[9m"
+
+	End              = "\033[0m"
+	EndBold          = "\033[22m"
+	EndDim           = "\033[22m"
+	EndItalic        = "\033[23m"
+	EndUnderline     = "\033[24m"
+	EndInverse       = "\033[27m"
+	EndHidden        = "\033[28m"
+	EndStrikethrough = "\033[29m"
 )
